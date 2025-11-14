edc8cbdb698a5197c04c93353d781a680224e0d1
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-11-15 05:22:43 +0600

    Alhamdulillah, playing with window_group

--- extension.js
+++ extension.js
@@ -31,6 +31,32 @@ export default class NotificationThemeExtension extends Extension {
 
     // journalctl -f -o cat SYSLOG_IDENTIFIER=testing-by-blueray453
     journal(`Enabled`);
+
+    let window_group = global.get_window_group();
+
+    // window_group.get_children().forEach((child, i) => {
+    //   journal(`Child ${i}: ${child}`);
+    // });
+
+    // window_group.get_children().forEach((actor, i) => {
+    //   let metaWin = actor.get_meta_window?.();   // optional chaining
+    //   if (metaWin) {
+    //     journal(`Child ${i}: ${metaWin.get_window_type()}`);
+    //   }
+
+    // });
+
+    window_group.get_children().forEach(function (actor, i) {
+      if (typeof actor.get_meta_window === 'function') {
+        let metaWin = actor.get_meta_window();
+
+        // Single if statement checking both
+        if (metaWin && metaWin.get_window_type() === 0) {
+          let wmClass = (typeof metaWin.get_wm_class === 'function') ? metaWin.get_wm_class() : 'N/A';
+          journal(`Child ${i}: ${wmClass}`);
+        }
+      }
+    });
   }
 
   disable() {
