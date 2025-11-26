bda4a9d99b14b44e6f53440cbe471e30bcc73828
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-11-26 20:30:50 +0600

    Alhamdulillah, Sample to Create a Custom Signal

--- extension.js
+++ extension.js
@@ -1,94 +1,37 @@
 import GLib from 'gi://GLib';
-
-import Clutter from 'gi://Clutter';
 import GObject from 'gi://GObject';
-import St from 'gi://St';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
-
 import { setLogging, setLogFn, journal } from './utils.js'
 
-class MyPanelButton extends PanelMenu.Button {
+class SignalEmitter extends GObject.Object {
 
   static {
-    GObject.registerClass(this);
+    GObject.registerClass({
+      Signals: {
+        'custom-clicked': {
+          param_types: [
+            GObject.TYPE_STRING,  // message
+            GObject.TYPE_INT      // code
+          ]
+        }
+      }
+    }, this);
   }
 
   constructor() {
-    super(0.0, 'MyPanelButton');
+    super(); // ← THIS IS THE FIX - call parent constructor
+  }
 
-    // Create an icon
-    this._icon = new St.Icon({ icon_name: 'system-run-symbolic', style_class: 'system-status-icon' });
-    this.add_child(this._icon);
-
-    // Handle left and right click
-    this.connect('button-press-event', (actor, event) => {
-      let button = event.get_button();
-      if (button === Clutter.BUTTON_PRIMARY) { // left click
-        journal(`Left click detected!`);
-        // Clear existing menu items first
-        this.menu.removeAll();
-
-        // Create and add menu items
-        let item1 = new PopupMenu.PopupMenuItem('Left Menu Item 1');
-        let item2 = new PopupMenu.PopupMenuItem('Left Menu Item 2');
-
-        // Connect activation signals
-        item1.connect('activate', () => {
-          journal('Left Menu Item 1 clicked!');
-        });
-
-        item2.connect('activate', () => {
-          journal('Left Menu Item 2 clicked!');
-        });
-
-        // Add items to menu
-        this.menu.addMenuItem(item1);
-        this.menu.addMenuItem(item2);
-
-        // // Add a separator (optional)
-        // this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-
-        // Open the menu
-        this.menu.open(true);
-      } else if (button === Clutter.BUTTON_SECONDARY) { // right click
-        journal(`Right click detected!`);
-        // Clear existing menu items first
-        this.menu.removeAll();
-
-        // Create and add menu items
-        let item1 = new PopupMenu.PopupMenuItem('Right Menu Item 1');
-        let item2 = new PopupMenu.PopupMenuItem('Right Menu Item 2');
-
-        // Connect activation signals
-        item1.connect('activate', () => {
-          journal('Right Menu Item 1 clicked!');
-        });
-
-        item2.connect('activate', () => {
-          journal('Right Menu Item 2 clicked!');
-        });
-
-        // Add items to menu
-        this.menu.addMenuItem(item1);
-        this.menu.addMenuItem(item2);
-
-        // // Add a separator (optional)
-        // this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-
-        // Open the menu
-        this.menu.open(true);
-      }
-      return Clutter.EVENT_STOP; // prevent default
-    });
+  triggerClick(message = "Default message", code = 0) {
+    this.emit('custom-clicked', message, code);
   }
 }
 
 export default class MyExtension extends Extension {
   constructor(metadata) {
     super(metadata);
+    this._emitter = null;
+    this._handlerId = null;
   }
 
   enable() {
@@ -116,14 +59,28 @@ export default class MyExtension extends Extension {
     // journalctl -f -o cat SYSLOG_IDENTIFIER=testing-by-blueray453
     journal(`Enabled`);
 
-    this.myButton = new MyPanelButton();
-    Main.panel.addToStatusArea('my-button', this.myButton);
+    this._emitter = new SignalEmitter();
+
+    this._handlerId = this._emitter.connect('custom-clicked', (obj, msg, code) => {
+      // Use journal instead of print for logging
+      journal(`Signal object: ${obj}`);
+      journal(`Signal received: ${msg}, code: ${code}`);
+    });
+
+    // Trigger clicks with proper parameters
+    this._emitter.triggerClick("First click", 1);
+    this._emitter.triggerClick("Second click", 2);
+    this._emitter.triggerClick("Third click", 3);
+
   }
 
   disable() {
-    if (this.myButton) {
-      this.myButton.destroy();
-      this.myButton = null;
+    if (this._emitter && this._handlerId) {
+      this._emitter.disconnect(this._handlerId);
+      this._emitter = null;
+      this._handlerId = null;
     }
+
+    journal(`Disabled`);
   }
 }
