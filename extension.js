b1b860dc3c68c095fc0aec39923fb6a28879537e
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-12-03 09:20:05 +0600

    Alhamdulillah, progress

--- extension.js
+++ extension.js
@@ -1,106 +1,51 @@
-import St from 'gi://St';
-import Clutter from 'gi://Clutter';
+import Meta from 'gi://Meta';
+import Shell from 'gi://Shell';
+import GLib from 'gi://GLib';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
-let button;
-let leftMenu;
-let rightMenu;
-let manager;
+import { setLogging, setLogFn, journal } from './utils.js'
 
-export default class Extension {
+export default class ExampleExtension extends Extension {
   enable() {
-    // -------------------------
-    // Create the panel button
-    // -------------------------
-    button = new St.Button({
-      label: 'Hello',
-      style_class: 'panel-button',
-      reactive: true,
-      can_focus: true,
-      track_hover: true,
-    });
-
-    Main.panel._centerBox.insert_child_at_index(button, 0);
-
-    // -------------------------
-    // Popup menu manager
-    // -------------------------
-    manager = new PopupMenu.PopupMenuManager(button);
-
-    // -------------------------
-    // Left-click menu
-    // -------------------------
-    leftMenu = new PopupMenu.PopupMenu(button, 0.5, St.Side.TOP);
-    Main.layoutManager.uiGroup.add_child(leftMenu.actor);
-    manager.addMenu(leftMenu);
-
-    let leftItemA = new PopupMenu.PopupMenuItem('Left Option A');
-    leftItemA.connect('activate', () => Main.notify('Left Menu', 'A selected'));
-    leftMenu.addMenuItem(leftItemA);
-
-    let leftItemB = new PopupMenu.PopupMenuItem('Left Option B');
-    leftItemB.connect('activate', () => Main.notify('Left Menu', 'B selected'));
-    leftMenu.addMenuItem(leftItemB);
-
-    // -------------------------
-    // Right-click menu
-    // -------------------------
-    rightMenu = new PopupMenu.PopupMenu(button, 0.5, St.Side.TOP);
-    Main.layoutManager.uiGroup.add_child(rightMenu.actor);
-    manager.addMenu(rightMenu);
-
-    let rightItemX = new PopupMenu.PopupMenuItem('Right Option X');
-    rightItemX.connect('activate', () => Main.notify('Right Menu', 'X selected'));
-    rightMenu.addMenuItem(rightItemX);
-
-    let rightItemY = new PopupMenu.PopupMenuItem('Right Option Y');
-    rightItemY.connect('activate', () => Main.notify('Right Menu', 'Y selected'));
-    rightMenu.addMenuItem(rightItemY);
-
-    // -------------------------
-    // Handle clicks
-    // -------------------------
-    button.connect('button-press-event', (actor, event) => {
-      const btn = event.get_button();
-
-      // Close both menus before opening the one you want
-      leftMenu.close();
-      rightMenu.close();
-
-      if (btn === 1) {
-        leftMenu.open();
-        return Clutter.EVENT_STOP; // stop propagation
-      }
-      if (btn === 3) {
-        rightMenu.open();
-        return Clutter.EVENT_STOP; // stop propagation
+    setLogFn((msg, error = false) => {
+      let level;
+      if (error) {
+        level = GLib.LogLevelFlags.LEVEL_CRITICAL;
+      } else {
+        level = GLib.LogLevelFlags.LEVEL_MESSAGE;
       }
 
-      return Clutter.EVENT_PROPAGATE;
+      GLib.log_structured(
+        'testing-by-blueray453',
+        level,
+        {
+          MESSAGE: `${msg}`,
+          SYSLOG_IDENTIFIER: 'testing-by-blueray453',
+          CODE_FILE: GLib.filename_from_uri(import.meta.url)[0]
+        }
+      );
     });
+
+    setLogging(true);
+
+    // journalctl -f -o cat SYSLOG_IDENTIFIER=testing-by-blueray453
+    journal(`Enabled`);
+
+    this._settings = this.getSettings('org.gnome.shell.extensions.testing-by-blueray453');
+
+    Main.wm.addKeybinding(
+      'toggle-action',
+      this._settings,
+      Meta.KeyBindingFlags.NONE,
+      Shell.ActionMode.ALL,
+      () => {
+        log('Super+T pressed!');
+      }
+    );
   }
 
   disable() {
-    if (manager) {
-      manager.removeMenu(leftMenu);
-      manager.removeMenu(rightMenu);
-      manager = null;
-    }
-
-    if (leftMenu) {
-      leftMenu.destroy();
-      leftMenu = null;
-    }
-
-    if (rightMenu) {
-      rightMenu.destroy();
-      rightMenu = null;
-    }
-
-    if (button) {
-      button.destroy();
-      button = null;
-    }
+    this._settings = null;
   }
 }
