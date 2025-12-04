a2c2b41eae975d6e8e2a557278509d0837a4fd20
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-12-04 17:58:12 +0600

    Alhamdulillah, working

--- extension.js
+++ extension.js
@@ -3,13 +3,26 @@ import St from 'gi://St';
 import GLib from 'gi://GLib';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import GObject from 'gi://GObject';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
 import { setLogging, setLogFn, journal } from './utils.js'
 
 const Display = global.get_display();
 const WorkspaceManager = global.get_workspace_manager();
 
-export default class ExampleExtension extends Extension {
+export default class MyPanelButton extends PanelMenu.Button {
+
+  static {
+    GObject.registerClass(this);
+  }
+
+  constructor() {
+    super(0.0, 'MyPanelButton');
+    this._icon = new St.Icon({ icon_name: 'system-run-symbolic', style_class: 'system-status-icon' });
+    this.add_child(this._icon);
+  }
+
   enable() {
     setLogFn((msg, error = false) => {
       let level;
@@ -35,18 +48,13 @@ export default class ExampleExtension extends Extension {
     // journalctl -f -o cat SYSLOG_IDENTIFIER=testing-by-blueray453
     journal(`Enabled`);
 
-    button = new St.Button({
-      style_class: 'system-menu-action',
-      label: 'Check Windows',
-      reactive: true,
-      can_focus: true,
+    this._icon.connect('button-press-event', (actor, event) => {
+      this._logWindowInfo();
     });
-
-    button.connect('clicked', this._logWindowInfo());
 
     // Add button to the top-right UI for testing
     // Main.layoutManager.addChrome(button);
-    Main.panel._centerBox.insert_child_at_index(container, 0);
+    // Main.panel._centerBox.insert_child_at_index(container, 0);
   }
 
   _logWindowInfo(){
