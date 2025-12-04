a3feb25ee080de49c35370d5aae477febd118c0f
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-12-04 17:31:25 +0600

    Alhamdulillah, working

--- extension.js
+++ extension.js
@@ -1,5 +1,5 @@
 import Meta from 'gi://Meta';
-import Shell from 'gi://Shell';
+import St from 'gi://St';
 import GLib from 'gi://GLib';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
@@ -35,13 +35,35 @@ export default class ExampleExtension extends Extension {
     // journalctl -f -o cat SYSLOG_IDENTIFIER=testing-by-blueray453
     journal(`Enabled`);
 
+    button = new St.Button({
+      style_class: 'system-menu-action',
+      label: 'Check Windows',
+      reactive: true,
+      can_focus: true,
+    });
 
+    button.connect('clicked', this._logWindowInfo());
+
+    // Add button to the top-right UI for testing
+    // Main.layoutManager.addChrome(button);
+    Main.panel._centerBox.insert_child_at_index(container, 0);
+  }
+
+  _logWindowInfo(){
     let windows_by_stacking = Display.sort_windows_by_stacking(global.get_window_actors().map(actor => actor.meta_window).filter(win => win.get_window_type() === Meta.WindowType.NORMAL));
+
+    journal('windows_by_stacking:');
+    windows_by_stacking.forEach(win => journal(`${win.get_title()}`));
+
     let tab_list = Display.get_tab_list(0, WorkspaceManager.get_active_workspace());
+
+    tab_list.forEach(win => journal(`${win.get_title()}`));
+
     let window_group = global.get_window_group();
+    journal('window_group:', window_group);
+
     let top_window_group = global.get_top_window_group();
-
-
+    journal('top_window_group:', top_window_group);
   }
 
   disable() {
